b1f40c1f1db6cd6b01d0313f973b575debdc05c7
adrianaj20 <adrian.najarro@tecsup.edu.pe>
2023-05-21 23:18:40 -0500

    s7

---
+++ XLab7/XLab7.Android/Services/QrScanningService.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Nfc.Tech;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XLab7.Services;
+using XLab7.Droid.Services;
+using ZXing.Mobile;
+
+
+[assembly: Dependency(typeof(QrScanningService))]
+namespace XLab7.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            MobileBarcodeScanner.Initialize(MainActivity.Instance.Application);
+
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
+        }
+    }
+}
